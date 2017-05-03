c550983b797777824a4bf92249dd8b56d533d9d2
nstauffer <ngstauffer@gmail.com>
2017-05-03 07:00:10 -0600

    Internal consistency and refactor for flexibility

--- R/attribute.shapefile.R
+++ R/attribute.shapefile.R
@@ -1,71 +1,87 @@
 #' Attributing a Spatial Data Frame using another Spatial Data Frame
 #'
 #' This function will take a Spatial Points/Polygons Data Frame and add one attribute fields from a second Spatial Points/Polygons Data Frame
-#' @param shape1 A Spatial Points/Polygons Data Frame containing the geometry to add an attribute to
-#' @param shape2 A Spatial Points/Polygons Data Frame containing the geometry to add an attribute from
-#' @param attributefield The name of the field in \code{shape2} as a string containing the values to add to \code{shape1}
-#' @param newfield The name of the field in \code{shape1} as a string to add the values from \code{shape2$attributefield} to. If NULL, the field will use \code{attributefield}. Defaults to NULL.
-#' @param projection An \code{sp::CRS()} argument to apply in the event that \code{shape1} and \code{shape2} have different projections. Defaults to \code{CRS("+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")}
-#' @return The original SPDF shape1 with the new field containing the values inherited from shape2.
+#' @param spdf1 A Spatial Points/Polygons Data Frame containing the geometry to add an attribute to
+#' @param spdf2 A Spatial Points/Polygons Data Frame containing the geometry to add an attribute from
+#' @param attributefield The name of the field in \code{spdf2} as a string containing the values to add to \code{spdf1}
+#' @param newfield The name of the field in \code{spdf1} as a string to add the values from \code{spdf2$attributefield} to. If NULL, the field will use \code{attributefield}. Defaults to NULL.
+#' @param projection An \code{sp::CRS()} argument to apply in the event that \code{spdf1} and \code{spdf2} have different projections. Defaults to \code{CRS("+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")}
+#' @return The original SPDF spdf1 with the new field containing the values inherited from spdf2.
 #' @example
 #' attribute.shapefile()
 
 
-attribute.shapefile <- function(shape1,
-                                shape2,
+attribute.shapefile <- function(spdf1,
+                                spdf2,
                                 attributefield = NULL,
                                 newfield = NULL,
                                 projection = CRS("+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")
                                 ){
-  if (is.null(attributefield) | !(attributefield %in% names(shape2@data))) {
-    stop("attributefield must be a field name found in shape2")
+  if (is.null(attributefield) | !(attributefield %in% names(spdf2@data))) {
+    stop("attributefield must be a field name found in spdf2")
   }
 
   if (is.null(newfield)) {
     newfield <- attributefield
   }
 
-  if (shape1@proj4string@projargs != shape2@proj4string@projargs) {
+  remove.coords <- F
+  coord.names <- spdf1@coords %>% colnames()
+
+  if (spdf1@proj4string@projargs != spdf2@proj4string@projargs) {
     ## Make sure that the points also adhere to the same projection
-    shape1 <- shape1 %>% spTransform(projection)
-    shape2 <- shape2 %>% spTransform(projection)
+    spdf1 <- spdf1 %>% sp::spTransform(projection)
+    spdf2 <- spdf2 %>% sp::spTransform(projection)
+  } else {
+    projection <- spdf1@proj4string
   }
 
   ## Initialize list for attributed SPDFs
-  attributed.spdfs <- list()
+  attributed.dfs <- list()
 
   ## We'll check each attribute field value independently
-  for (n in unique(shape2@data[, attributefield])) {
+  for (n in unique(spdf2@data[, attributefield])) {
     ## Create a copy of the points to work with on this loop
-    current.shape1 <- shape1
+    current.spdf <- spdf1
     ## Get the data frame from over()
-    over.result <- over(current.shape1,
-                        shape2[shape2@data[, attributefield] == n, ])
+    over.result <- sp::over(current.spdf,
+                        spdf2[spdf2@data[, attributefield] == n, ])
     ## Add the values to the newfield column
-    current.shape1@data[, newfield] <- over.result[, attributefield]
+    current.spdf@data[, newfield] <- over.result[, attributefield]
+    if (!(coord.names[1] %in% names(current.spdf@data)) & !(coord.names[2] %in% names(current.spdf@data))){
+      current.spdf@data <- cbind(current.spdf@data, current.spdf@coords)
+      remove.coords <- T
+    }
     ## Make sure that the polygons have unique IDs
-    if (class(current.shape1) == "SpatialPolygonsDataFrame") {
-      current.shape1 <- spChFIDs(current.shape1,
+    if (class(current.spdf) == "SpatialPolygonsDataFrame") {
+      current.spdf <- sp::spChFIDs(current.spdf,
                                  paste(runif(n = 1, min = 0, max = 666666666),
-                                       row.names(current.shape1),
+                                       row.names(current.spdf),
                                        sep = "."))
     }
+    current.df <- current.spdf@data
 
     ## Only if the number of coordinates is greater than 0!
-    print(nrow(current.shape1[!is.na(current.shape1@data[, newfield]), ]))
-    if (nrow(current.shape1[!is.na(current.shape1@data[, newfield]), ]) > 0) {
-      attributed.spdfs <- list(attributed.spdfs,
-                               current.shape1[!is.na(current.shape1@data[, newfield]), ])
+    print(nrow(current.df[!is.na(current.df[, newfield]), ]))
+    if (nrow(current.df[!is.na(current.df[, newfield]), ]) > 0) {
+      attributed.dfs[[paste(n)]] <- current.df[!is.na(current.df[, newfield]), ]
     }
   }
 
-  if (length(attributed.spdfs) > 0) {
-    if (length(attributed.spdfs) == 1) {
-      output <- get(attributed.spdfs[1])
+  if (length(attributed.dfs) > 0) {
+    if (length(attributed.dfs) == 1) {
+      output <- attributed.dfs[[1]] %>%
+        sp::SpatialPointsDataFrame(data = .,
+                                  coords = .[, coord.names],
+                                  proj4string = projection)
     } else {
-      output <- eval(parse(text = paste0("rbind(`",
-                                         paste(attributed.spdfs, collapse = "`,`"),
-                                         "`)")))
+      output <- dplyr::bind_rows(attributed.dfs) %>%
+        sp::SpatialPointsDataFrame(data = .,
+                                   coords = .[, coord.names],
+                                   proj4string = projection)
+    }
+    if (remove.coords) {
+      output <- output[, names(output)[!(names(output) %in% coord.names)]]
     }
   } else {
     output <- NULL

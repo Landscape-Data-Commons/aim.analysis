39d8015e891de706cdad3576ff12de84bc3ce549
nstauffer <ngstauffer@gmail.com>
2017-05-04 11:02:57 -0600

    Update documentation

--- R/qc.R
+++ R/qc.R
@@ -1,4 +1,8 @@
 #' Confirming that design database points flagged as sampled have valid primary keys
+#'
+#' @description Returns a data frame of all the primary keys and plot IDs of plots in imported design databases that had a valid primary key but were not flagged as sampled or were flagged as sampled but didn't have a valid primary key.
+#' @param dd.raw The output from \code{read.dd()}.
+#' @param target.values A character vector of the point fates considered sampled. Will always include \code{"Target Sampled"} and \code{"TS"}.
 #' @export
 validate.keys <- function(dd.raw, target.values = c("Target Sampled",
                                                     "TS")){

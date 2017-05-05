10c239de63eba900ef3533a9a0d8115c63019693
nstauffer <ngstauffer@gmail.com>
2017-05-05 07:53:09 -0600

    Comment

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,7 @@
 .onLoad <- function(libname, pkgname){
+  ## Because piping within the functios won't work unless this is attached
   library(tidyverse)
+  ## If arcgisbinding is installed and R is 32-bit, then it needs to be attached and arc.check_product() run to get it set up
   if ("arcgisbinding" %in% rownames(installed.packages()) & !grepl(x = R.Version()$arch, pattern = "^x86_64")) {
     library(arcgisbinding)
     arc.check_product()

633d6f21550cc73d06891da8e81e19241966d8c3
nstauffer <ngstauffer@gmail.com>
2017-06-30 10:59:11 -0600

    Flip default to adjustedweights = FALSE

--- R/analyzing.R
+++ R/analyzing.R
@@ -5,7 +5,7 @@
 #' @param point.weights Data frame output from \code{weight()}, equivalent to \code{weight()[["point.weights"]]} or \code{weight()[[2]]}.
 #' @param default.reportingunit A string to populate the reporting unit field with in the case that designs were not restricted by reporting unit. Defaults to \code{"No reporting unit"}
 #' @param reportingunit.type A string to populate the reporting unit type field with. Depends on the reporting units used, e.g. it may be \code{"Watershed"} or \code{"Sage-grouse Habitat"}. Defaults to \code{NA}.
-#' @param adjustedweights Logical. If \code{TRUE}, use the values in the field ADJWGT instead of the field WGT. Allows for quick comparison between the results of analysis with and without the weight adjustment. Defaults to \code{TRUE}.
+#' @param adjustedweights Logical. If \code{TRUE}, use the values in the field ADJWGT instead of the field WGT. Allows for quick comparison between the results of analysis with and without the weight adjustment. Defaults to \code{FALSE}.
 #' @return A data frame with the output from \code{spsurvey::cat.analysis()}
 #' @keywords analysis
 #' @examples
@@ -16,7 +16,7 @@ analyze <- function(evaluated.points,
                      point.weights,
                      default.reportingunit = "No reporting unit",
                      reportingunit.type = NA,
-                     adjustedweights = TRUE
+                     adjustedweights = FALSE
                      ){
 
   ## Sanitization
@@ -27,6 +27,10 @@ analyze <- function(evaluated.points,
   evaluated.points <- evaluated.points %>% dplyr::filter(grepl(x = PRIMARYKEY, pattern = "^[0-9]{15,24}-[0-9]{1,3}-[0-9]{1,3}$"))
   if (is.null(reportingunit.type)){
     reportingunit.type <- NA
+  }
+
+  if (!adjustedweights) {
+    message("adjustedweights is FALSE and therefore weights adjusted for reporting units will not be used.")
   }
 
   ## Then we combine them!

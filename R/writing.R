2fc0b034c4d15941a06467e50db9e59019839d5c
nstauffer <ngstauffer@gmail.com>
2017-07-14 10:45:25 -0600

    Correctly handle lists of multiple data frames

--- R/writing.R
+++ R/writing.R
@@ -20,14 +20,23 @@ filename.aim <- function(name,
 #' Writing out analysis outputs as a .csv
 #'
 #' @description Creates a \code{.csv} file composed of the output from one or more executions of \code{analyze()}. If combining multiple analyze() outputs, do so as a list with \code{c(analyze(dataset1), analyze(dataset2), ...)}.
-#' @param analysis.output Either a single output from \code{analyze()} or a list of multiple outputs, e.g. \code{c(analyze(dataset1), analyze(dataset2))}.
+#' @param analysis.output Either a single output from \code{analyze()} or a list of multiple outputs, e.g. \code{list(analyze(dataset1), analyze(dataset2))}.
 #' @param name A string of the project name, e.g. "Idaho_SageGrouse"
 #' @param out.path A string specifying the output folder path
 #' @export
 write.analysis <- function(analysis.output,
                            name,
                            out.path){
-  output <- dplyr::bind_rows(analysis.output[grep(names(analysis.output), pattern = "^analyses$")])
+  if (class(analysis.output) == "data.frame") {
+    output <- analysis.output
+  } else if (class(analysis.output) == "list") {
+    if (length(analysis.output > 1)) {
+      output <- dplyr::bind_rows(analysis.output[grep(names(analysis.output), pattern = "^analyses$")])
+    } else {
+      output <- analysis.output[[1]]
+    }
+  }
+
   write.csv(output, paste(out.path, filename.aim(name, "analysis", "csv"), sep = "/"))
 }
 

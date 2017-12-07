b6554304ebeac1c485d057d7c3d8439831cf38d0
nstauffer <ngstauffer@gmail.com>
2017-12-07 14:09:37 -0800

    Attach only dplyr, not the whole tidyverse

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname){
-  ## Because piping within the functios won't work unless this is attached
-  library(tidyverse)
+  ## Because piping within the functions won't work unless this is attached
+  library(dplyr)
   ## If arcgisbinding is installed and R is 32-bit, then it needs to be attached and arc.check_product() run to get it set up
   if ("arcgisbinding" %in% rownames(installed.packages()) & !grepl(x = R.Version()$arch, pattern = "^x86_64")) {
     library(arcgisbinding)

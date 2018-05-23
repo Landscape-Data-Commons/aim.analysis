945e1c9af4ccf3a212d26d097bb93ed339ff457e
nstauffer <ngstauffer@gmail.com>
2018-05-23 15:47:58 -0700

    Change data.path default to getwd()

--- R/reading.R
+++ R/reading.R
@@ -10,13 +10,18 @@
 #' @export
 
 ## TODO: Add capitalization sanitization stuff
-read.benchmarks <- function(data.path = "",
+read.benchmarks <- function(data.path = NULL,
                             benchmarks.filename = "",
                             sheet.name = "Monitoring Objectives",
                             indicator.lut = NULL,
                             indicator.lut.benchmarkfield = "indicator.name",
                             convert.l2r = TRUE
 ){
+  ## Use the working directory if none is provided
+  if (is.null(data.path)){
+    data.path <- getwd()
+  }
+
   ## Check for the file extension
   if (!grepl(benchmarks.filename, pattern = "\\.((xlsx)|(csv)|(xls))", ignore.case = TRUE)) {
     stop("The benchmark filename needs to have a valid file extension (xlsx, csv, or xls). The most likely extension is xlsx.")

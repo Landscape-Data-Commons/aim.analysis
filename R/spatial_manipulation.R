34804d8b25512c3c6af6d5e560a4cc2d36cf95c3
nstauffer <ngstauffer@gmail.com>
2018-07-16 07:37:32 -0700

    Use correct object name

--- R/spatial_manipulation.R
+++ R/spatial_manipulation.R
@@ -29,7 +29,7 @@ attribute.shapefile <- function(spdf1,
 
   if (any(is.na(spdf2@data[[attributefield]]))) {
     message(paste0("Removing geometry from spdf2 with NA in the field ", attributefield))
-    spdf2 <- spdf2[!is.na(spdf2@data[[eval.strata.field]]),]
+    spdf2 <- spdf2[!is.na(spdf2@data[[attributefield]]),]
   }
 
   if (is.null(newfield)) {

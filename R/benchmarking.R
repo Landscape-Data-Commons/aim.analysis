fbc4b4ca8d57e2f62a451099a3932e44e9866ed2
Nelson Stauffer <ngstauffer@gmail.com>
2024-11-15 11:38:16 -0800

    Update benchmarking functions

--- R/benchmarking.R
+++ R/benchmarking.R
@@ -1,138 +1,196 @@
 #' Apply benchmarks to data
 #' @description Using data that has unique identifier and indicator value variables, apply benchmarks. The benchmarks are applied to the data based on the benchmark group(s) that the data belong to, which may already be assigned in the data or may be added with a benchmark group lookup table.
-#' @param data Data frame or spatial * data frame. The data to be benchmarked. Must contain the variable(s) in \code{idvars} as unique identifiers. May be "wide" or "long" data. If wide, then each indicator name should be a variable name. If long, then the indicator names and values should be in variables named \code{"indicator_var"} and \code{"value"}.
-#' @param idvars Character string or vecor of character strings. The names of the variable(s) in \code{data} which serve as unique identifiers.
-#' @param benchmark_group_var Optional character string. The name of the variable that holds the benchmark group memberships. If \code{NULL} then it defaults to the variable in \code{benchmark_group_lut} that is not in \code{idvars}. Defaults to \code{NULL}.
-#' @param benchmark_group_lut Optional data frame. The lookup table used to assign benchmark group membership to \code{data}. Must contain all of the variables in \code{idvar} and one additional variable with the benchmark group memberships.
-#' @param benchmarks Data frame. The benchmarks in the format produced by \code{read.benchmarks()}. If the benchmark group membership variable in other data frames is not \code{"Benchmark.Group"} then it must be renamed here.
-#' @param verbose Logical. If \code{TRUE} then the function will generate additional messages as it executes.
-#' @param tdat_version Version of TerrADat used in the analysis for use in \code{indicator lookup()}. Defaults to \code{"2"}.
+#' @param data Data frame. The data to be benchmarked. Must contain the variable(s) in \code{data_id_vars} as unique identifiers. May be "wide" or "long" data. If wide, then each indicator name should be a variable name. If long, then the indicator names and values should be in variables named \code{"indicator_var"} and \code{"value"}.
+#' @param benchmarks Data frame. The benchmarks in the format produced by \code{read_benchmarks()}.
+#' @param data_id_vars Character string or vecor of character strings. The names of the variable(s) in \code{data} which serve as unique identifiers.
+#' @param data_group_var Optional character string. The name of the variable that holds the benchmark group memberships. If \code{NULL} then it defaults to the variable in \code{benchmark_group_var}. Defaults to \code{NULL}.
+#' @param benchmark_group_var Character string. The name of the variable that holds the benchmark group memberships. Defaults to \code{"Benchmark_Group"}.
+#' @param benchmark_indicator_var Character string. The name of the variable that holds the indicator names in the benchmarks. Defaults to \code{"Indicator"}.
+# #' @param verbose Logical. If \code{TRUE} then the function will generate additional messages as it executes.
+#' @param use_tdat_indicator_lookup Logical. Set to \code{TRUE} if the the values in variable \code{benchmark_indicator_var} are the standard human-readable names for the indicators and the corresponding variables in \code{data} use the variable names from the Terrestrial AIM Database. Defaults to \code{TRUE}.
+# #' @param indicator_lookup_path Optional character string. The path to the folder where the lookup tables to be read in with
 #' @return A long data frame of each point/benchmarked indicator combination with the assigned condition category.
 #' @export
 apply_benchmarks <- function(data,
-                             idvars,
-                             benchmark_group_var = NULL,
-                             benchmark_group_lut = NULL,
                              benchmarks,
-                             verbose = FALSE,
-                             tdat_version = "2") {
-  if (!any(c("sf", "data.frame" %in% class(data)))) {
+                             data_id_vars,
+                             data_group_var = NULL,
+                             # benchmark_idvars = NULL,
+                             benchmark_group_var = "Benchmark_Group",
+                             benchmark_indicator_var = "Indicator",
+                             use_tdat_indicator_lookup = TRUE) {
+  #### Benchmark sanitization --------------------------------------------------
+  if (!("data.frame" %in% class(benchmarks))) {
+    stop("Benchmarks must be a data frame.")
+  }
+
+  required_benchmark_vars <- c(benchmark_indicator_var)
+  missing_benchmark_vars <- setdiff(x = required_benchmark_vars,
+                                    y = names(benchmarks))
+  if (length(missing_benchmark_vars) > 0) {
+    stop(paste0("The following variables are missing from benchmarks: ",
+                paste(missing_benchmark_vars,
+                      collapse = ", ")))
+  }
+  benchmarks <- dplyr::rename(.data = benchmarks,
+                              indicator_name = tidyselect::matches(match = benchmark_indicator_var))
+
+  # This needs to be reconsidered for data moving forward.
+  if (use_tdat_indicator_lookup) {
+    indicator_lut <- indicator_lookup(tdat_version = 2)
+    names(indicator_lut) <- c("indicator_var", "indicator_name")
+    benchmarks <- dplyr::left_join(x = benchmarks,
+                                   y = indicator_lut,
+                                   by = "indicator_name",
+                                   relationship = "many-to-one") |>
+      dplyr::mutate(.data = _,
+                    indicator_var = dplyr::case_when(is.na(indicator_var) ~ indicator_name,
+                                                     .default = indicator_var))
+  } else {
+    benchmarks <- dplyr::rename(.data = benchmarks,
+                                indicator_var = indicator_name)
+  }
+
+  if (is.null(benchmark_group_var)) {
+    benchmarks <- dplyr::mutate(.data = benchmarks,
+                                benchmark_group = "All")
+  } else {
+    if (!(benchmark_group_var %in% names(benchmarks))) {
+      stop(paste("The benchmark_group_var variable", benchmark_group_var, "does not appear in the benchmarks."))
+    }
+    benchmarks <- dplyr::rename(.data = benchmarks,
+                                benchmark_group = tidyselect::matches(match = benchmark_group_var))
+  }
+
+  #### Data sanitization -------------------------------------------------------
+  if (!("data.frame" %in% class(data))) {
     stop("data must either be a data frame or a spatial data frame.")
   }
   data <- sf::st_drop_geometry(x = data)
 
-  idvars <- c(idvars, benchmark_group_var)
-  missing_idvars <- idvars[!(idvars %in% names(data))]
-  if (length(missing_idvars) > 0) {
-    stop(paste0("The following variables are missing from data: ", paste(missing_idvars, collapse = ", ")))
+  required_data_vars <- c(data_id_vars, data_group_var)
+  missing_data_vars <- setdiff(x = required_data_vars,
+                               y = names(data))
+  if (length(missing_data_vars) > 0) {
+    stop(paste0("The following variables are missing from data: ",
+                paste(missing_data_vars,
+                      collapse = ", ")))
   }
-
-  if (class(benchmarks) != "data.frame") {
-    stop("Benchmarks must be a data frame.")
-  }
-
-  if (!is.null(benchmark_group_var)) {
-    if (!(benchmark_group_var %in% names(benchmarks)) & !("Benchmark.Group" %in% names(benchmarks))) {
-      stop(paste0("The variable ", benchmark_group_var, "does not appear in the data frame benchmarks"))
-    } else {
-      if (verbose) {
-        message(paste0("Adding the contents of benchmarks[['Benchmark.Group']] to benchmarks[['", benchmark_group_var, "']]"))
-      }
-      benchmarks[[benchmark_group_var]] <- benchmarks[["Benchmark.Group"]]
-    }
-  }
-
-  if (is.null(benchmark_group_var) & is.null(benchmark_group_lut)) {
-    stop("Either benchmark_group_var or benchmark_group_lut must not be NULL")
-  } else if (!is.null(benchmark_group_var) & !is.null(benchmark_group_lut)) {
-    if (verbose) {
-      message("Both a banchmark variable and a benchmark group lookup table have been provided. Assuming that the benchmark_group_var is in the lookup table and will be applied to the data from there.")
-    }
-    if (!(benchmark_group_var) %in% names(benchmark_group_lut)) {
-      stop(paste0("The variable ", benchmark_group_var, " does not appear in the names of benchmark_group_lut"))
-    }
-  } else if (!is.null(benchmark_group_var) & is.null(benchmark_group_lut)) {
-    if (!(benchmark_group_var) %in% names(data)) {
-      stop(paste0("The variable ", benchmark_group_var, " does not appear in the names of data. Did you mean to supply a lookup table as well?"))
-    }
-  }
-
-  if (!is.null(benchmark_group_lut)) {
-    common_vars_benchmarks <- names(table(c(names(benchmarks), names(benchmark_group_lut))))[table(c(names(benchmarks), names(benchmark_group_lut))) > 1]
-    if (length(common_vars_benchmarks) < 1) {
-      stop("There are no variables in common between benchmark_group_lut and benchmarks")
-    }
-
-    if (!is.null(benchmark_group_var)) {
-      if (verbose) {
-        message("Removing existing benchmark group variable from data to replace with values from lookup table.")
-      }
-      data <- data[, names(data)[!(names(data %in% benchmark_group_var))]]
-    }
-    common_vars <- names(table(c(names(data), names(benchmark_group_lut))))[table(c(names(data), names(benchmark_group_lut))) > 1]
-    if (verbose) {
-      message(paste0("Joining data and benchmark group lut by the variable(s): ", paste(common_vars, collapse = ", ")))
-    }
-    data_grouped <- dplyr::left_join(x = data,
-                                     y = benchmark_group_lut,
-                                     by = common_vars,
-                                     relationship = "one-to-one")
+  if (is.null(data_group_var)) {
+    data <- dplyr::mutate(.data = data,
+                          benchmark_group = "All")
   } else {
-    data_grouped <- data
+    data <- dplyr::rename(.data = data,
+                          benchmark_group = tidyselect::matches(match = data_group_var))
   }
 
-  if (verbose) {
-    message("Applying benchmarks")
+  #### Data checks -------------------------------------------------------------
+  if (!any(data$benchmark_group %in% benchmarks$benchmark_group) | !any(benchmarks$benchmark_group %in% data$benchmark_group)) {
+    stop("None of these data qualify for any of these benchmarks according to the benchmark groups. Double check your data_group_var and benchmark_group_var values.")
+  }
+  unrepresented_data_groups <- setdiff(x = unique(data$benchmark_group),
+                                       y = unique(benchmarks$benchmark_group))
+  if (length(unrepresented_data_groups) > 0) {
+    warning("Some data have no applicable benchmarks and will not be evaluted.")
+  }
+  unrepresented_benchmark_groups <- setdiff(x = unique(benchmarks$benchmark_group),
+                                            y = unique(data$benchmark_group))
+  if (length(unrepresented_benchmark_groups) > 0) {
+    warning("Some benchmarks have no applicable data and will not be evaluted.")
   }
 
-  # Get the benchmarks merged to the points
-  indicator_lut <- indicator.lookup(tdat_version)
-  names(indicator_lut) <- c("indicator_var", "indicator_name")
-  benchmarks <- dplyr::left_join(x = benchmarks,
-                                 y = indicator_lut,
-                                 by = c("indicator" = "indicator_name"),
-                                 relationship = "many-to-one")
+  benchmarks <- dplyr::filter(.data = benchmarks,
+                              benchmark_group %in% data$benchmark_group)
 
-  eval_vars <- names(benchmarks)[grepl(names(benchmarks), pattern = "^evalstring\\d+$")]
+  # if (is.null(benchmark_idvars)) {
+  #   benchmarks_list <- list(benchmarks)
+  # } else {
+  # benchmarks_list <- split(x = benchmarks,
+  #                          f = benchmarks$Management_Question)
+  # }
 
-  idvars <- c(idvars, "Benchmark.Group")
+  data <- dplyr::filter(.data = data,
+                        benchmark_group %in% benchmarks$benchmark_group)
 
-  # Make the data tall
-  if (!all(c("indicator", "value") %in% names(data_grouped))) {
-    indicator_vars_present <- names(data_grouped)[names(data_grouped) %in% indicator_lut[["indicator_var"]]]
-    data_tall <- tidyr::pivot_longer(data = data_grouped[, c(idvars, indicator_vars_present)],
-                                     cols = tidyselect::one_of(indicator_vars_present),
-                                     names_to = "indicator",
-                                     values_to = "value")
-  } else {
-    data_tall <- data
+  indicator_variables_present <- intersect(x = unique(benchmarks$indicator_var),
+                                           y = names(data))
+  if (length(indicator_variables_present) == 0) {
+    stop("None of the indicators in the benchmarks appear as variables in the data. Double check use_tdat_indicator_lut, the indicator names in the benchmarks, and the variable names in the data.")
+  } else if (length(indicator_variables_present) != length(unique(benchmarks$indicator_var))) {
+    warning("Not all indicators in the benchmarks appear in the data.")
   }
 
-  data_benchmarked <- dplyr::left_join(x = data_tall,
-                            y = benchmarks,
-                            by = c("indicator" = "indicator_var",
-                                     "Benchmark.Group" = "Benchmark.Group",
-                                     "Reporting.Unit" = "Reporting.Unit"),
-                            relationship = "one-to-many")
+  benchmarks <- dplyr::filter(.data = benchmarks,
+                              indicator_var %in% indicator_variables_present)
 
-  benchmark_vector <- sapply(X = 1:nrow(data_benchmarked),
-                             data = data_benchmarked,
-                             eval_vars = eval_vars,
-                             FUN = function(X, data, eval_vars){
-                               all(sapply(X = eval_vars,
-                                          data = data[X, ],
-                                          FUN = function(X, data){
-                                            evalstring <- gsub(data[[X]][1],
-                                                               pattern = "(x){1}",
-                                                               replacement = data[["value"]])
-                                            eval(parse(text = evalstring))
-                                          }))
-                             })
+  #### Actual benchmarking -----------------------------------------------------
+  # Because sometimes the benchmarks are for different classes of values (e.g.,
+  # some character and some numeric), we'll do these one at a time.
+  results <- list()
+  for (current_management_question in unique(benchmarks$Management_Question)) {
+    current_benchmarks <- dplyr::filter(.data = benchmarks,
+                                        Management_Question == current_management_question)
+    for (current_indicator in unique(current_benchmarks$indicator_var)) {
+      data_tall <- dplyr::select(.data = data,
+                                 tidyselect::all_of(data_id_vars),
+                                 benchmark_group,
+                                 tidyselect::all_of(current_indicator)) |>
+        tidyr::pivot_longer(data = _,
+                            cols = tidyselect::all_of(current_indicator),
+                            names_to = "indicator_var",
+                            values_to = "value") |>
+        dplyr::inner_join(x = _,
+                          y = current_benchmarks,
+                          relationship = "many-to-many",
+                          by = c("benchmark_group",
+                                 "indicator_var"))
 
-  output <- dplyr::select(.data = data_benchmarked,
-                          tidyselect::all_of(idvars),
-                          indicator,
-                          Condition.Category)[benchmark_vector]
+      eval_vars <- names(current_benchmarks)[grepl(names(current_benchmarks), pattern = "^evalstring\\d+$")]
 
-  return(output)
+      benchmark_vector <- sapply(X = 1:nrow(data_tall),
+                                 data_tall = data_tall,
+                                 eval_vars = eval_vars,
+                                 FUN = function(X, data_tall, eval_vars){
+                                   all(sapply(X = eval_vars,
+                                              current_data = data_tall[X, ],
+                                              FUN = function(X, current_data){
+                                                current_value <- current_data[["value"]]
+                                                # This nonsense is so that strings
+                                                # have '' around them so that this
+                                                # evaluates correctly
+                                                if (class(current_value) == "character") {
+                                                  current_value <- paste0("'", current_value, "'")
+                                                  evalstring_vector <- stringr::str_split_1(string = current_data[[X]][1],
+                                                                                            pattern = "[ ]")
+                                                  target_value_index <- setdiff(x = c(1, 3),
+                                                                                y = which(evalstring_vector == "x"))
+                                                  evalstring_vector[target_value_index] <- paste0("'", evalstring_vector[target_value_index], "'")
+                                                  evalstring <- paste(evalstring_vector,
+                                                                      collapse = " ")
+                                                } else {
+                                                  evalstring <- current_data[[X]][1]
+                                                }
+                                                evalstring <- gsub(evalstring,
+                                                                   pattern = "(x){1}",
+                                                                   replacement = current_value)
+                                                eval(parse(text = evalstring))
+                                              }))
+                                 })
+
+      results[[paste0(current_management_question,
+                      "_",
+                      current_indicator)]] <- dplyr::select(.data = data_tall,
+                                                            tidyselect::all_of(data_id_vars),
+                                                            indicator = indicator_var,
+                                                            Condition_Category)[benchmark_vector, ] |>
+        dplyr::mutate(.data = _,
+                      Management_Question = current_management_question)
+    }
+  }
+  dplyr::bind_rows(results) |>
+    # dplyr::filter(.data = _,
+    #               # Figure out why these are necessary!!!!
+    #               !is.na(indicator),
+    #               !is.na(Condition_Category))
+    dplyr::distinct()
 }
